e0ca854f6210a80c6653b7d123de99f61354d74a
Thomas <thomas.schallert@gmx.at>
2024-04-24 19:22:26 +0200

    added testimage & imported it to gallery

--- src/Gallery.xaml.cs
+++ src/Gallery.xaml.cs
@@ -5,5 +5,9 @@ public partial class Gallery : ContentPage
 	public Gallery()
 	{
 		InitializeComponent();
-	}
+        BackgroundColor = Color.FromArgb("#333333");
+        Image image = new Image();
+        image.Source = "testbild_strand.jpeg";
+        galleryStackLayout.Children.Add(image);
+    }
 }
