e3e72c312f6a022a61b579fda89579259715fde1
Thomas <thomas.schallert@gmx.at>
2024-05-05 15:58:37 +0200

    Load and save gallery system + test images

--- src/Gallery.xaml.cs
+++ src/Gallery.xaml.cs
@@ -1,45 +1,88 @@
-namespace LomaPro;
-
-public partial class Gallery : ContentPage
+namespace LomaPro
 {
-    public List<Image_gal> imageList = new List<Image_gal>();
-
-	public Gallery()
-	{
-        InitializeComponent();
-        BackgroundColor = Color.FromArgb("#333333");
-        
-        
-
-    }
-    public async void addImage()
+    public partial class Gallery : ContentPage
     {
-        try
-        {
-            var results = await FilePicker.PickMultipleAsync(new PickOptions
-            {
-                PickerTitle = "Select images"
-            });
+        public List<Image_gal> imageList = new List<Image_gal>();
 
-            if (results != null)
+        public Gallery()
+        {
+            InitializeComponent();
+            BackgroundColor = Color.FromArgb("#333333");
+            LoadImages();
+        }
+        
+
+        public void LoadImages()
+        {
+
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string projectDirectory = Directory.GetParent(baseDirectory).Parent.Parent.Parent.Parent.Parent.FullName;
+            string imagesFolder = Path.Combine(projectDirectory, "Resources", "Images");
+
+
+
+
+
+
+            if (Directory.Exists(imagesFolder))
             {
-                foreach (var result in results)
+                var imageFiles = Directory.GetFiles(imagesFolder, "*.jpeg").Concat(Directory.GetFiles(imagesFolder, "*.png"));
+                if (galleryScrollView.Content is StackLayout stackLayout)
                 {
-                    string filePath = result.FullPath;
-                    string fileName = Path.GetFileName(filePath);
-                    Image_gal image = new Image_gal("Description", filePath, fileName);
+                stackLayout.Children.Clear();
+                }
+
+                foreach (var imagePath in imageFiles)
+                {
+                    string fileName = Path.GetFileName(imagePath);
+                    Image_gal image = new Image_gal("Description", imagePath, fileName);
+                    imageList.Add(image);
+                }
+                foreach (Image_gal image in imageList)
+                {
                     image.drawImage(galleryScrollView);
                 }
             }
         }
-        catch (Exception ex)
+
+        public async void addImage()
         {
-            // Handle any exceptions
-            Console.WriteLine($"Exception: {ex}");
+            try
+            {
+                var results = await FilePicker.PickMultipleAsync(new PickOptions
+                {
+                    PickerTitle = "Select images",
+                    FileTypes = FilePickerFileType.Images
+                });
+
+                if (results != null)
+                {
+                    string destinationFolder = @"C:\Users\thoma\OneDrive\Dokumente\Schule\2 AHIF FÄCHER\POS\Loma\Loma\src\Resources\Images";
+
+                    foreach (var result in results)
+                    {
+                        string filePath = result.FullPath;
+                        string fileName = Path.GetFileName(filePath);
+                        string destinationPath = Path.Combine(destinationFolder, fileName);
+
+                        // Kopieren der ausgewählten Datei an den Zielort
+                        File.Copy(filePath, destinationPath, overwrite: true);
+
+                        Image_gal image = new Image_gal("Description", destinationPath, fileName);
+                        image.drawImage(galleryScrollView);
+                    }
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex}");
+            }
         }
-    }
-    private void Button_Clicked(object sender, EventArgs e)
-    {
-        addImage();
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            addImage();
+        }
     }
 }
