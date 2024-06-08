58dd6ce3d417a8fd3e32ebdfb7286b67991fdbe5
Thomas <thomas.schallert@gmx.at>
2024-06-08 13:12:31 +0200

    Logging bug fix

--- src/Logging.cs
+++ src/Logging.cs
@@ -1,19 +1,17 @@
 ﻿using Serilog;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using Serilog.Core;
+using System.Reflection;
 
 namespace LomaPro
 {
     public class Logging
     {
         static public Logger logger = new LoggerConfiguration()
-            .WriteTo.File("log-.txt", rollingInterval:
-            RollingInterval.Day, retainedFileCountLimit: 15).CreateLogger();
-
+            .WriteTo.File(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "log-.txt"),
+                          rollingInterval: RollingInterval.Day,
+                          retainedFileCountLimit: 15)
+            .CreateLogger();
     }
-
 }
