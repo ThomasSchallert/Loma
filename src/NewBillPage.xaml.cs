25ca6100460535fe584070b0004c1712f8da8c25
Merlin <82440231+Merlin1212@users.noreply.github.com>
2024-06-09 17:15:42 +0200

    error fix in tab

--- src/NewBillPage.xaml.cs
+++ src/NewBillPage.xaml.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -6,143 +6,118 @@ using System.Reflection;
 using System.Text.Json;
 using Microsoft.Maui.Controls;
 
-namespace LomaPro;
-
-public partial class NewBillPage : ContentPage
+namespace LomaPro
 {
-    public List<Group> groups = new List<Group>();
-    public Artikel artikel = new Artikel();
-
-    private List<BillGroup> billGroups;
-    public NewBillPage()
+    public partial class NewBillPage : ContentPage
     {
-        InitializeComponent();
-    }
+        public List<Group> groups = new List<Group>();
+        public Artikel artikel = new Artikel();
 
-    public void UpdateUI(List<Group> groups)
-    {
-        this.groups = groups;
-        GroupStackLayout.Children.Clear();
-
-        foreach (var group in groups)
+        public NewBillPage()
         {
-            var frame = new Frame
+            InitializeComponent();
+        }
+
+        public void UpdateUI(List<Group> groups)
+        {
+            this.groups = groups;
+            GroupStackLayout.Children.Clear();
+
+            foreach (var group in groups)
             {
-                BorderColor = Microsoft.Maui.Graphics.Colors.LightGray,
-                CornerRadius = 5,
-                Padding = 10,
-                Margin = 10
-            };
+                var frame = new Frame
+                {
+                    BorderColor = Microsoft.Maui.Graphics.Colors.LightGray,
+                    CornerRadius = 5,
+                    Padding = 10,
+                    Margin = 10
+                };
 
-            var grid = new Grid();
-            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
-            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
-            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+                var grid = new Grid();
+                grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+                grid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+                grid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
 
-            var nameLabel = new Label { Text = group.Name };
+                var nameLabel = new Label { Text = group.Name };
 
-            var picker = new Picker
-            {
-                Title = "Beteiligte Personen",
-                ItemsSource = Enumerable.Range(0, group.Size + 1).Select(i => i.ToString()).ToList(),
-                HorizontalOptions = LayoutOptions.End,
-                SelectedIndex = 0
-            };
-            group.picker = picker;
+                var picker = new Picker
+                {
+                    Title = "Beteiligte Personen",
+                    ItemsSource = Enumerable.Range(0, group.Size + 1).Select(i => i.ToString()).ToList(),
+                    HorizontalOptions = LayoutOptions.End,
+                    SelectedIndex = 0
+                };
+                group.picker = picker;
+                grid.Children.Add(nameLabel);
+                Grid.SetColumn(nameLabel, 0);
 
-            var payerPicker = new Picker
-            {
-                Title = "Bezahlt von",
-                ItemsSource = groups.Select(g => g.Name).ToList(), 
-                HorizontalOptions = LayoutOptions.End,
-                SelectedIndex = 0
-            };
+                grid.Children.Add(picker);
+                Grid.SetColumn(picker, 2);
 
-            grid.Children.Add(nameLabel);
-            Grid.SetColumn(nameLabel, 0);
+                frame.Content = grid;
 
-            grid.Children.Add(picker);
-            Grid.SetColumn(picker, 1);
-
-            grid.Children.Add(payerPicker);
-            Grid.SetColumn(payerPicker, 2);
-
-            frame.Content = grid;
-
-            GroupStackLayout.Children.Add(frame);
-        }
-        Logging.logger.Information("Updated UI");
-
-    }
-
-
-    private async void OnOkClicked(object sender, EventArgs e)
-    {
-        double gesamtpreis = 0;
-        int howmany = 0;
-        try
-        {
-            gesamtpreis = Convert.ToDouble(BetragEntry.Text); 
-            artikel.Name = BezeichnungEntry.Text;
-            artikel.Price = gesamtpreis;
-            Logging.logger.Information("articel created");
-        }
-        catch (Exception ex)
-        {
-            Logging.logger.Error(ex, "Invalid datatype.");
-            await DisplayAlert("Error", "Please enter a valid number", "OK");
-            return;
-        }
-
-        foreach (var billGroup in billGroups)
-        {
-            billGroup.SelectedIndex = billGroup.picker.SelectedIndex;
-            howmany += billGroup.picker.SelectedIndex;
-        }
-
-        if (howmany == 0)
-        {
-            Logging.logger.Warning("No persons selected.");
-            await DisplayAlert("Error", "Please select at least one person", "OK");
-            Logging.logger.Warning("No persons selected.");
-            return;
-        }
-
-        double personhastopay = gesamtpreis / howmany;
-        double payed = 0;
-        foreach (var billGroup in billGroups)
-        {
-            billGroup.HasToPay += Math.Round((billGroup.picker.SelectedIndex) * personhastopay, 2);
-            int index = billGroup.picker.SelectedIndex;
-            payed += Math.Round((billGroup.picker.SelectedIndex) * personhastopay, 2);
-
-            if (billGroup.Name == billGroup.PayerPicker.SelectedItem.ToString())
-            {
-                billGroup.PaidAmount += gesamtpreis;
+                GroupStackLayout.Children.Add(frame);
             }
-            else
-            {
-                billGroup.PaidAmount -= Math.Round((billGroup.picker.SelectedIndex) * personhastopay, 2);
-            }
+            Logging.logger.Information("Updated UI");
         }
-        Logging.logger.Information("Updated price");
 
-        if (payed != gesamtpreis)
+        private async void OnOkClicked(object sender, EventArgs e)
         {
-            Random random = new Random();
-            groups[random.Next(groups.Count)].HasToPay += Math.Round(gesamtpreis - payed,2);
-            billGroups[random.Next(billGroups.Count)].HasToPay += gesamtpreis - payed;
-            Logging.logger.Warning("Adjustment made for rounding differences.");
+            double gesamtpreis = 0;
+            int howmany = 0;
+            try
+            {
+                gesamtpreis = Convert.ToDouble(BetragEntry.Text);
+                artikel.Name = BezeichnungEntry.Text;
+                artikel.Price = gesamtpreis;
+                Logging.logger.Information("articel created");
+            }
+            catch (Exception ex)
+            {
+                Logging.logger.Error(ex, "Invalid datatype.");
+                await DisplayAlert("Error", "Please enter a valid number", "OK");
+                return;
+            }
+
+            foreach (var group in groups)
+            {
+                group.SelectedIndex = group.picker.SelectedIndex; // Speichere den ausgew�hlten Index
+                howmany += group.picker.SelectedIndex;
+            }
+
+            if (howmany == 0)
+            {
+                Logging.logger.Warning("No persons selected.");
+                await DisplayAlert("Error", "Please select at least one person", "OK");
+                Logging.logger.Warning("No persons selected.");
+                return;
+            }
+
+            double personhastopay = gesamtpreis / howmany;
+            double payed = 0;
+            foreach (var group in groups)
+            {
+                group.HasToPay += Math.Round((group.picker.SelectedIndex) * personhastopay, 2);
+                int index = group.picker.SelectedIndex;
+                payed += Math.Round((group.picker.SelectedIndex) * personhastopay, 2);
+            }
+            Logging.logger.Information("Updated price");
+
+            if (payed != gesamtpreis)
+            {
+                Random random = new Random();
+                groups[random.Next(groups.Count)].HasToPay += Math.Round(gesamtpreis - payed, 2);
+                Logging.logger.Warning("Adjustment made for rounding differences.");
+            }
+
+
+            await Navigation.PopAsync();
         }
-        
 
-        await Navigation.PopAsync();
-    }
-
-
-    private async void OnCancelClicked(object sender, EventArgs e)
-    {
-        await Navigation.PopAsync();
-        Logging.logger.Information("Canceled");
+        private async void OnCancelClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+            Logging.logger.Information("Canceled");
+        }
     }
 }
